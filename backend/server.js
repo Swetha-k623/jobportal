5ac746a8816f9b4187d44729f04dab6df309f5b1
Swetha-k623 <hariswe2731@gmail.com>
2025-04-10 14:14:00 +0530

    Updated form structure and script logic

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Job = require("./models/Job"); 
+const path = require('path');
+
 
 const app = express();
 app.use(express.json()); 
@@ -46,3 +48,11 @@ const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
